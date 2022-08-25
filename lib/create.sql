269fe23a053adb66b5cc816f48bddb572fe712b8
Bernardngari <bernardngari.ke@gmail.com>
2022-08-25 13:47:25 +0300

    commit tree 05bd72f06a346208fab92df0c7aca44dfb768b2e

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bears (
-ID INTEGER PRIMARY KEY,
-Name TEXT,
-Age INTEGER,
-Sex TEXT,
-Color TEXT,
-Temperament TEXT,
-Alive TEXT
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
 );
